d8ad2125011f3c9013f1e84271cc74e74c728c70
Peter GAO <37321872+petergaobro@users.noreply.github.com>
2025-03-14 12:07:19 +1100

    connect and pass data from MS sql server

--- DotNet8-API-Entity/Controllers/PeterHeroController.cs
+++ DotNet8-API-Entity/Controllers/PeterHeroController.cs
@@ -1,6 +1,8 @@
-﻿using DotNet8_API_Entity.Entities;
+﻿using DotNet8_API_Entity.Data;
+using DotNet8_API_Entity.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection.Metadata.Ecma335;
 
 namespace DotNet8_API_Entity.Controllers
@@ -9,20 +11,19 @@ namespace DotNet8_API_Entity.Controllers
     [ApiController]
     public class PeterHeroController : ControllerBase
     {
+
+        // injection SSMS
+        private readonly DataContext _context;
+
+        public PeterHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<PeterHero>>> GetAllHeroes()
         {
-            var heroes = new List<PeterHero>
-            {
-                new PeterHero
-                {
-                    Id = 1,
-                    Name = "wiki",
-                    FirstName = "ziwei",
-                    LastName = "Ren",
-                    Place = "Kingston"
-                }
-            };
+            var heroes = await _context.PeterHeroes.ToListAsync();
             return Ok(heroes);
         }
     }
---
+++ DotNet8-API-Entity/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿
+using DotNet8_API_Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8_API_Entity.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) { 
+        
+        
+        
+        }
+
+        public DbSet<PeterHero> PeterHeroes { get; set; }
+    }
+}
