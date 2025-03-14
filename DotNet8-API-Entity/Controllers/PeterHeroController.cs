9a19acb7e92c9e9b5068dd9bae5882a71650de05
Peter GAO <37321872+petergaobro@users.noreply.github.com>
2025-03-14 11:27:15 +1100

    add API controller

---
+++ DotNet8-API-Entity/Controllers/PeterHeroController.cs
@@ -0,0 +1,29 @@
+﻿using DotNet8_API_Entity.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace DotNet8_API_Entity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeterHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<PeterHero>>> GetAllHeroes()
+        {
+            var heroes = new List<PeterHero>
+            {
+                new PeterHero
+                {
+                    Id = 1,
+                    Name = "wiki",
+                    FirstName = "ziwei",
+                    LastName = "Ren",
+                    Place = "Kingston"
+                }
+            };
+            return Ok(heroes);
+        }
+    }
+}
---
+++ DotNet8-API-Entity/Entities/PeterHero.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNet8_API_Entity.Entities
+{
+    public class PeterHero
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+
+    }
+}
